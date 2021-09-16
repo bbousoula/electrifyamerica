16f58ba5e64ba25b0346c375099e89106125ebad
Bradley Bousoula <bbousoul@proximitybbdo.fr>
2021-09-16 08:51:38 +0400

    dry js code

--- js/script.js
+++ js/script.js
@@ -1,18 +1,11 @@
 $(document).ready(function () {
   function search_hide() {
-    $(".search-box").toggle();
+    $(".close, .search, .search-box").toggle();
     if ($(window).width() > 1024) {
       $(".nav_desktop").toggle();
     }
   }
-  $(".search").click(function () {
-    $(".close").toggle();
-    $(".search").toggle();
-    search_hide();
-  });
-  $(".close").click(function () {
-    $(".search").toggle();
-    $(".close").toggle();
+  $(".search, .close").click(function () {
     search_hide();
   });
 });
